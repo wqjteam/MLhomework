0dd406d5af2070bbcef34fd23432fdd2bad6db44
githubwqj <893453172@qq.com>
2020-10-31 14:51:09 +0800

    改名

--- sklearn/src/org/wqj/learning/MLWorkHome/experiment1/GradientDescent.py
+++ sklearn/src/org/wqj/learning/MLWorkHome/experiment1/GradientDescent.py
@@ -1,1 +1,19 @@
 # coding=utf-8
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from numpy import genfromtxt
+dataPath = r"./Input/data1.csv"
+dataSet = pd.read_csv(dataPath,header=None)
+print(dataSet)
+price = []
+rooms = []
+area = []
+for data in range(0,len(dataSet)):
+    area.append(dataSet[0][data])
+    rooms.append(dataSet[1][data])
+    price.append(dataSet[2][data])
+
+def gradientDescent(rooms, price, area):
+    theta = []
+    return theta

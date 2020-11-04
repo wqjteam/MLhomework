cc00a57d291428e852095effe19105683f905c79
githubwqj2 <2812930196@qq.com>
2020-11-04 13:22:52 +0800

    111

--- sklearn/src/org/wqj/learning/MLWorkHome/experiment1/ActivePlot.py
+++ sklearn/src/org/wqj/learning/MLWorkHome/experiment1/ActivePlot.py
@@ -9,7 +9,7 @@ t = [0]
 t_now = 0
 m = [sin(t_now)]
 
-for i in range(2000):
+for i in range(100):
     # plt.clf() # 清空画布上的所有内容。此处不能调用此函数，不然之前画出的点，将会被清空。
     t_now = i*0.1
     """
@@ -17,8 +17,9 @@ for i in range(2000):
     为点'.'、'o'、'*'都可以，就是不能为线段'-'。因为一条线段需要两
     个点才能确定。第二种方法是scatter函数，也即画点。
     """
-    plt.plot(t_now,sin(t_now),c='r') # 第次对画布添加一个点，覆盖式的。
+    plt.plot(t_now,sin(t_now),'-r') # 第次对画布添加一个点，覆盖式的。
     # plt.scatter(t_now, sin(t_now))
 
-    plt.draw()#注意此函数需要调用
+    # plt.draw()#注意此函数需要调用
+    # plt.show()
     plt.pause(0.01)

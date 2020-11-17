022b8861a4ccf669085b4261dd75f809685409d1
githubwqj2 <2812930196@qq.com>
2020-11-17 18:34:13 +0800

    111

---
+++ sklearn/src/org/wqj/learning/MLWorkHome/experiment2/MyLinearRegression.py
@@ -0,0 +1,19 @@
+# coding=utf-8
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import os
+
+curPath = os.path.abspath(os.path.dirname(__file__))
+rootPath = curPath[:curPath.find("sklearn\\") + len("sklearn\\")]
+dataPath = rootPath + "Input/MLWorkHome/experiment2/data.csv"
+x_y_data = pd.read_csv(dataPath,header=1)
+year = []
+bus = []
+gdp = []
+for index in range(0, len(x_y_data)):
+    year.append(x_y_data[1][index])
+    bus.append(x_y_data[2][index])
+    gdp.append(x_y_data[3][index])
+print(year)

848ee9f1d3dcfa8c4e3805b4cf0149840278db2c
githubwqj2 <2812930196@qq.com>
2021-01-06 18:33:06 +0800

    提交运行命令

--- ultimatemlwork/yolov5/aggrresult.py
+++ ultimatemlwork/yolov5/aggrresult.py
@@ -1,23 +1,25 @@
-
-
 import glob
 import os
+
 curPath = os.path.abspath(os.path.dirname(__file__))
 rootPath = curPath[:curPath.find("ultimatemlwork\\") + len("ultimatemlwork\\")]
-label_path = rootPath+"\\yolov5\\runs\\detect\\exp18\\"
-jpg_path = rootPath+"\\yolov5\\runs\\detect\\exp18\\"
+
+maxepoch = ""
+for f in os.listdir(rootPath + "\\yolov5\\runs\\detect\\"):
+    fp = f
+    max = -1
+    if ((int(fp.replace("exp", ""))) > max):
+        max = int(fp.replace("exp", ""))
+        maxepoch = f
+label_path = rootPath + "\\yolov5\\runs\\detect\\" + maxepoch
+jpg_path = rootPath + "\\yolov5\\runs\\detect\\" + maxepoch
 result_file = glob.glob(label_path + '*.txt')
-#jpg_file = glob.glob(jpg_path + '*.jpg')
 
-f = open(rootPath+"\\yolov5\\result.txt","w")
-
-
+f = open(rootPath + "\\yolov5\\result.txt", "w")
 
 check_list = []
 for file in result_file:
-    result = open(file,'r')
+    result = open(file, 'r')
     for line in result:
         f.write(line)
 f.close()
-
-
